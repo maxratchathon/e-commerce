7c00d4aa37462bc22be7970b6d53cbed66f25055
Maxmerock123 <Maxmerock@gmail.com>
2023-11-30 10:52:06 +0700

    remove bootstrap and tailwind from css, add homepage with vanila css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, Nav, Navbar, Card } from 'react-bootstrap'
+
 
 
 function App() {
   return (
     <>
-      <Navbar bg="light" data-bs-theme="light" className="nav-container">
-        <Container>
-          <Nav >
-            <Nav.Link href="#home">SHOP ALL</Nav.Link>
-            <Nav.Link href="#features">PURIFICATION</Nav.Link>
-            <Nav.Link href="#pricing">HOME</Nav.Link>
-            <Navbar.Brand className="" href="#home">LARQ</Navbar.Brand>
-            <Nav.Link href="#pricing">TECHNOLOGY</Nav.Link>
-            <Nav.Link href="#pricing">FAQ</Nav.Link>
-            <Nav.Link href="#pricing">[icon]</Nav.Link>
-            <Nav.Link href="#pricing">[icon]</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <div className='pic-container bg-dark d-flex' style={{ height: "500px" }}>
-        <h3 className='text-white text-center'>Input picture here</h3>
+      <div className='navbar-container'>
+        <div>
+          <h2>EverShop</h2>
+        </div>
+        <div>
+          <a>Men</a>
+          <a>Women</a>
+        </div>
+        <div>
+          <a>[icon]</a>
+          <a>[icon]</a>
+          <a>[icon]</a>
+        </div>
       </div>
 
-      <div className='d-flex gap-2 justify-content-center m-5'>
-      <Card>
-          <Card.Body>
-            <Card.Title>Brilliant</Card.Title>
-            <Card.Subtitle>****</Card.Subtitle>
-            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Subtitle>subtitle</Card.Subtitle>
-            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Subtitle>subtitle</Card.Subtitle>
-            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Subtitle>subtitle</Card.Subtitle>
-            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Subtitle>subtitle</Card.Subtitle>
-            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Card.Text>
-          </Card.Body>
-        </Card>
-
+      <div>
+        <div style={{ height: "300px", backgroundColor: "#EEEEEE" }}>
+          [imageframe]
+        </div>
       </div>
+
+      <div className="card-container">
+        <div>
+          <div className="img-card" style={{ height: "300px", width: "350px", backgroundColor: "#CCCCCC" }} />
+          <h3>MEN SHOES COLLECTION</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <button type="button">button</button>
+        </div>
+        <div>
+          <div className="img-card" style={{ height: "300px", width: "350px", backgroundColor: "#CCCCCC" }} />
+          <h3>WOMEN SHOES COLLECTION</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <button type="button">button</button>
+        </div>
+        <div>
+          <div className="img-card" style={{ height: "300px", width: "350px", backgroundColor: "#CCCCCC" }} />
+          <h3>MEN SHOES COLLECTION</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <button type="button">button</button>
+        </div>
+      </div>
+
+      <div className="shoes-container">
+        <div>
+          <div className="img-card" style={{ height: "250px", width: "250px", backgroundColor: "#CCCCCC" }} />
+          <h3>Shoes name</h3>
+          <p>$ 425.50</p>
+          <button type="button">button</button>
+        </div>
+        <div>
+          <div className="img-card" style={{ height: "250px", width: "250px", backgroundColor: "#CCCCCC" }} />
+          <h3>Shoes name</h3>
+          <p>$ 425.50</p>
+          <button type="button">button</button>
+        </div>
+        <div>
+          <div className="img-card" style={{ height: "250px", width: "250px", backgroundColor: "#CCCCCC" }} />
+          <h3>Shoes name</h3>
+          <p>$ 425.50</p>
+          <button type="button">button</button>
+        </div>
+      </div>
+
+      <div className="footer">
+        <p>[logo]</p>
+        <p>[logo]</p>
+        <p>[logo]</p>
+      </div>
+
+
     </>
   );
 }
