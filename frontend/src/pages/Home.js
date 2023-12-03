7fd6374109234029dee73ae41e634810a72c2109
Maxmerock123 <Maxmerock@gmail.com>
2023-12-04 06:16:59 +0700

    change global css to Home.module.css for Homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,78 +1,73 @@
-import React from 'react'
-import shoesBg from '../images/shoes-bg.jpg'
-import menShop from '../images/men-shop.jpg'
-import womenShop from '../images/women-shop.jpg'
-import kidsShop from '../images/kids-shop.jpg'
-import style from './Home.module.css'
-
-
+import React from 'react';
+import shoesBg from '../images/shoes-bg.jpg';
+import menShop from '../images/men-shop.jpg';
+import womenShop from '../images/women-shop.jpg';
+import kidsShop from '../images/kids-shop.jpg';
+import style from './Home.module.css';
 
 const Home = () => {
     return (
         <>
-            <div className='hero-image' style={{ backgroundImage: `url(${shoesBg})` }}>
+            <div className={style.heroImage} style={{ backgroundImage: `url(${shoesBg})` }}>
                 <a href='#' />
-                <div className='hero-text'>
+                <div className={style.heroText}>
                     <h1>Discount 20% For All Orders Over $2000</h1>
-                    <h3>Use coupon codeDISCOUNT20 </h3>
+                    <h3>Use coupon code DISCOUNT20 </h3>
                     <h3>Use coupon DISCOUNT20</h3>
                 </div>
             </div>
 
-            <div className="card-container">
-                <div className='card'>
-                    <img className='shop-img' src={kidsShop} />
+            <div className={style.cardContainer}>
+                <div className={style.card}>
+                    <img className={style.shopImg} src={kidsShop} alt="Kids Shop" />
                     <h3>KIDS SHOES COLLECTION</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                     <a href='kids'>
-                        <button className='shop-btn' type="button">SHOP KIDS</button>
+                        <button className={style.shopBtn} type="button">SHOP KIDS</button>
                     </a>
                 </div>
-                <div className='card'>
-                    <img className='shop-img' src={menShop} />
+                <div className={style.card}>
+                    <img className={style.shopImg} src={menShop} alt="Men Shop" />
                     <h3>MEN SHOES COLLECTION</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                     <a href='men'>
-                        <button className='shop-btn' type="button">SHOP MENS</button>
-
+                        <button className={style.shopBtn} type="button">SHOP MENS</button>
                     </a>
                 </div>
-                <div className='card'>
-                    <img className='shop-img' src={womenShop} />
+                <div className={style.card}>
+                    <img className={style.shopImg} src={womenShop} alt="Women Shop" />
                     <h3>WOMEN SHOES COLLECTION</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                     <a href='women'>
-                        <button className='shop-btn' type="button">SHOP WOMEN</button>
+                        <button className={style.shopBtn} type="button">SHOP WOMEN</button>
                     </a>
                 </div>
             </div>
 
-            <div className="shoes-container">
-                <div className='shoes-card'>
-                    <div className="img-card" />
-                    <h3 className='shoes-name'>Nike air zoom pegasus 35</h3>
-                    <p className='shoes-price'>$ 425.50</p>
+            <div className={style.shoesContainer}>
+                <div className={style.shoesCard}>
+                    <div className={style.imgCard} />
+                    <h3 className={style.shoesName}>Nike air zoom pegasus 35</h3>
+                    <p className={style.shoesPrice}>$ 425.50</p>
                 </div>
-                <div className='shoes-card'>
-                    <div className="img-card" />
-                    <h3 className='shoes-name'>Mix and match chuck taylor all star</h3>
-                    <p className='shoes-price'>$ 425.50</p>
+                <div className={style.shoesCard}>
+                    <div className={style.imgCard} />
+                    <h3 className={style.shoesName}>Mix and match chuck taylor all star</h3>
+                    <p className={style.shoesPrice}>$ 425.50</p>
                 </div>
-                <div className='shoes-card'>
-                    <div className="img-card" />
-                    <h3 className='shoes-name'> Geography class chuck taylor all star</h3>
-                    <p className='shoes-price'>$ 425.50</p>
+                <div className={style.shoesCard}>
+                    <div className={style.imgCard} />
+                    <h3 className={style.shoesName}>Geography class chuck taylor all star</h3>
+                    <p className={style.shoesPrice}>$ 425.50</p>
                 </div>
-                <div className='shoes-card'>
-                    <div className="img-card" />
-                    <h3 className='shoes-name'> Geography class chuck taylor all star</h3>
-                    <p className='shoes-price'>$ 425.50</p>
+                <div className={style.shoesCard}>
+                    <div className={style.imgCard} />
+                    <h3 className={style.shoesName}>Geography class chuck taylor all star</h3>
+                    <p className={style.shoesPrice}>$ 425.50</p>
                 </div>
             </div>
-
-
         </>
-    )
+    );
 }
 
-export default Home
+export default Home;
