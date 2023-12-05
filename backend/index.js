8bec21061303917baf52c5a0ee18c2a7caeb2bbe
Maxmerock123 <Maxmerock@gmail.com>
2023-12-06 05:56:40 +0700

    add json server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express')
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,12 @@ const app = express();
 app.get("/api",(req,res) => {
     res.json({ message:"Hello World from server!" })
 });
+
+app.get("/comment",(req,res) => {
+    fetch("./db.json")
+    .then(result = res.json());
+    console.log(`message are: ${result}`)
+})
 
 
 
